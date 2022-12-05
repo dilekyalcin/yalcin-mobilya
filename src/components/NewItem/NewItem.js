7cbf1d18d58a56cb11758e0aad67a197b5749fdb
cangnss <cgunes52@gmail.com>
2022-12-05 21:22:11 +0300

    redesign items

--- src/components/NewItem/NewItem.js
+++ src/components/NewItem/NewItem.js
@@ -3,13 +3,22 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea, Grid } from "@mui/material";
-import mobilya from "../assets/image/yemekmasası1.jpg";
-import mobilya1 from "../assets/image/yemekmasası2.jpg";
-import mobilya2 from "../assets/image/yemekmasası3.jpeg";
-import mobilya3 from "../assets/image/yemekmasası4.jpg";
-import mobilya4 from "../assets/image/yemekmasası5.jpg";
-import mobilya5 from "../assets/image/yemekmasası6.jpeg";
+import { CardActionArea, Divider, Grid } from "@mui/material";
+import mobilya from "../../assets/image/yemekmasası1.jpg";
+import mobilya1 from "../../assets/image/yemekmasası2.jpg";
+import mobilya2 from "../../assets/image/yemekmasası3.jpeg";
+import mobilya3 from "../../assets/image/yemekmasası4.jpg";
+import mobilya4 from "../../assets/image/yemekmasası5.jpg";
+import mobilya5 from "../../assets/image/yemekmasası6.jpeg";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const data = [
   {
@@ -84,35 +93,45 @@ const data = [
   },
 ];
 
-const yemekMasalari = () => {
+const items = data.map((item) => {
+  return (
+    <SwiperSlide style={{ padding: "3rem 3rem"}}>
+      <Card sx={{ width: "75%", height:"auto" }}>
+        <CardActionArea>
+          <CardMedia component="img" height="140" image={item.imagePath} />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {item.sofaName}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item.sofaDesc}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </SwiperSlide>
+  );
+});
+
+const NewItem = () => {
   return (
     <div>
+      <h1>En Yeniler</h1>
       <Grid container direction="row" spacing={2} sx={{ margin: "auto" }}>
-            {data.map((item) => {
-              return (
-                <Grid item md={3}>
-                  <Card sx={{ maxWidth: 345 }}>
-                    <CardActionArea>
-                      <CardMedia
-                        component="img"
-                        height="140"
-                        image={item.imagePath}
-                      />
-                      <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                          {item.sofaName}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                          {item.sofaDesc}
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                </Grid>
-              );
-            })}
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          slidesPerView={4}
+        //   spaceBetween={50}
+          navigation
+          pagination={{ clickable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          {items}
+        </Swiper>
       </Grid>
+      <Divider></Divider>
     </div>
   );
 };
-export default yemekMasalari;
+export default NewItem;
